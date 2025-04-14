da977a2e5f81acff65db6b80b4ddc5de4ba75ca6
aless133 <aless133@yandex.ru>
2025-04-14 22:56:45 +0300

    refactor: use cookie-session

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,24 +1,20 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express, { Router, Request, Response, NextFunction } from 'express';
-import session from 'express-session';
+import cookieSession from 'cookie-session';
 
 export const getSession = () => {
   const SESSION_SECRET = process.env.SESSION_SECRET;
   if (!SESSION_SECRET) throw new Error('SESSION_SECRET not found');
-  return session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    },
+  return cookieSession({
+    name: 'session',
+    keys: [SESSION_SECRET],    
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
   });
 };
 
 export const authGuard = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.session.auth) {
+  if (!req.session?.auth) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
   next();
@@ -30,7 +26,7 @@ export function authRouter() {
   const router = Router();
   router.use(express.json());
   router.post('/login', (req: Request, res: Response) => {
-    if (req.body.password === APP_PASSWORD) {
+    if (req.session && req.body.password === APP_PASSWORD) {
       req.session.auth = true;
       return res.json({ success: true });
     }
--- src/types.d/types.d.ts
+++ src/types.d/types.d.ts
@@ -1,7 +1,27 @@
-declare module 'express-session' {
-  interface SessionData {
-    auth: boolean;
-  }
-}
+// declare module 'express-session' {
+//   interface SessionData {
+//     auth: boolean;
+//   }
+// }
+
+
+// declare module 'cookie-session' {
+//   interface SessionData {
+//     auth: boolean;
+//   }
+// }
+
+// import 'express'; // Extends Express types
+
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       session: {
+//         auth?: boolean;  // Your custom session data
+//         [key: string]: any; // Allow other properties
+//       } | null;
+//     }
+//   }
+// }
 
 export {}; //need to be module
