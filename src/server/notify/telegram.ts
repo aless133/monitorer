88104e0dfc121acf3046a859a616e8e55c3a6eeb
Aless133 <aless133@yandex.ru>
2025-04-16 16:28:17 +0300

    refactor: use fetch in telegram

--- src/server/notify/telegram.ts
+++ src/server/notify/telegram.ts
@@ -3,46 +3,33 @@ import https from 'node:https';
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_CHAT_ID;
 
-function request(method:string, data: any) {
+async function request(method: string, data: any) {
+  const url = `https://api.telegram.org/bot${token}/${method}`;
   const postData = JSON.stringify(data);
-  const options = {
-    hostname: 'api.telegram.org', 
-    path: '/bot' + token + '/' + method,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(postData),
-    },
-  };
 
-  const req = https.request(options, (res) => {
-    let data = '';
-
-    res.on('data', (chunk) => {
-      data += chunk;
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: postData,
     });
 
-    res.on('end', () => {
-      try {
-      	const json = JSON.parse(data);
-      	if (json.ok === true) {
-      		console.info("Telegram request was successful!");
-      		if (process.env.NODE_ENV != "prod") console.info(json);
-      	} else {
-      		console.error("Telegram request failed. Check the response:", json);
-      	}
-      } catch (error) {
-      	console.error("Telegram error parsing JSON:", error);
-      }
-    });
-  });
-
-  req.on('error', (error) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const json = await response.json();
+    if (json.ok === true) {
+      console.info("Telegram request was successful!");
+      if (process.env.NODE_ENV !== "prod") console.info(json);
+    } else {
+      console.error("Telegram request failed. Check the response:", json);
+    }
+    return json;
+  } catch (error) {
     console.error('Telegram request error:', error);
-  });
-
-  req.write(postData);
-  req.end();
+  }
 }
 
 function sendMessage(text: string) {
