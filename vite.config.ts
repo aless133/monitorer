774ba1ace75eee02e6d6688197b527c7d2427983
aless133 <aless133@yandex.ru>
2025-11-23 16:19:02 +0300

    feat: es2022

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2022' // or 'esnext'
+  },  
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
